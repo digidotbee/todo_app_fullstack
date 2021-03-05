3b2a5614c72247a3adcbb6496b9aa3a86a74d98b
Punchcode <punchcode@Punchcodes-MBP.fios-router.home>
2021-03-05 17:26:18 -0500

    adding thomas code

--- server/db-creation.js
+++ server/db-creation.js
@@ -1,9 +1,3 @@
-// https://stackoverflow.com/questions/28558920/postgresql-foreign-key-syntax
-// Users:
-// id
-// email
-// password
-// salt
 import sha512 from 'js-sha512'
 import conn from './db.js'
 import { createSalt } from './utils/auth.js'
@@ -11,6 +5,7 @@ import { createSalt } from './utils/auth.js'
 // due to foreign key reference issue during deletion
 const tables = [
     'users',
+    'todos',
 ]
 async function main() {
     for (let table of tables) {
@@ -25,9 +20,16 @@ async function main() {
         table.string('password', 128)
         table.string('salt', 20)
     })
+    await conn.schema.createTable(`todos`, (table) => {
+        table.increments('id')
+        table.string('description', 45)
+        table.enu('status', ['active', 'completed'])
+        table.integer('user_id').unsigned()
+        table.foreign('user_id').references('users.id')
+    })
     const salt = createSalt(20)
     await conn('users').insert({username: 'test', password: sha512('test' + salt), salt: salt})
+    await conn('todos').insert({description: 'my todo', status: 'active', user_id: 1})
     process.exit()
 }
 main()
-// respective sql below...
