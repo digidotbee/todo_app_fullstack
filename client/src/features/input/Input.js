e28efe6ad9d244038ea6a8c6f0249b706bcc34be
Candice James-Waples <76015275+CTJW@users.noreply.github.com>
2021-03-07 13:48:16 -0500

    Create Slice

--- client/src/features/input/Input.js
+++ client/src/features/input/Input.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React from "react";
 import { Button } from "antd";
 
 export default function Input() {
-    return (
-        <div>
-            <input
-                className="wrapper"
-                id="Todo"
-                type="text"
-                name="Todo"
-                placeholder="enter todo here"/>
-            <Button name="submit_todo" type="submit" class="submit">
-                submit
-            </Button>
-        </div>
-    )
+  const dispatch = useDispatch();
+  const todos = useSelector(selectTodos);
+  const [setTodo] = useState("");
+  function handleClick() {
+    dispatch(addTodo());
+  }
+
+  useEffect(() => {
+    dispatch(getTodos());
+  }, []);
+
+  return (
+    <div>
+      <form action="">
+        <input
+          className="wrapper"
+          id="Todo"
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          name="Todo"
+          placeholder="enter todo here"
+        />
+        <Button
+          onClick={handleClick}
+          name="submit_todo"
+          type="submit"
+          class="submit"
+        >
+          Add
+        </Button>
+      </form>
+      {/* {todos.map((todo) => {
+        return (
+          <div>
+            onClick={() => dispatch(postTodo(todo.id))}
+            onSubmit={(todo) => dispatch(updateTodo(todo.id))}
+          </div>
+        );
+      })} */}
+    </div>
+  );
 }
