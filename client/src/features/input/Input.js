16f753319f8057bb40c5f377d7485f0ab586213e
Jon-Nutman <jonnut730@gmail.com>
2021-03-07 12:52:45 -0500

    get and post working

--- client/src/features/input/Input.js
+++ client/src/features/input/Input.js
@@ -2,22 +2,22 @@ import {React, useState} from 'react'
 
 import { Button } from "antd";
 
-function handleSubmit(){
-    e.preventDefault()
+// function handleSubmit(){
+//     e.preventDefault()
 
-}
+// }
 
 export default function Input() {
     return (
         <div>
-           <form action="" onSubmit={handleSubmit}>
+           {/* <form action="" onSubmit={handleSubmit}>
           <input
           className='input'
             type="text"
             value={text}
             onChange={(e) => setText(e.target.value)}
           />
-        </form>
+        </form> */}
             <Button name="submit_todo" type="submit" class="submit">
                 submit
             </Button>
--- server/routes/todo_maker.js
+++ server/routes/todo_maker.js
@@ -1,17 +1,28 @@
 import express from 'express'
 const router = express.Router()
+import knex from '../db.js'
+
+router.get('/todos', async(req, res) => {
+    // const todos = await knex.raw('SELECT * FROM todos')
+    // const rows = todos.rows
+    // res.json(rows)
+    res.json({message:"test!!!"})
+})
 
 
-router.post('/db', async (req, res) => {
-    const {id, description, status, user_id} = req.body   
+router.post('/todos', async (req, res) => {
+    const {description, status, user_id} = req.body   
     await knex.raw(
 
 `
-INSERT INTO todos (id, description, status, user_id)
-VALUES (?,?,?,?)
+INSERT INTO todos (description, status, user_id)
+VALUES (?,?,?);
 `
 ,
-[id, description, status, user_id]
-    )
+
+[description, status, user_id]
+    );
     res.json({message: 'we know ya aint gon do it but its on the lsit'})
 })
+
+export default router
